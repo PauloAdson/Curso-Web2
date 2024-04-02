ecf1670563b10f445c2bf8b5f526220ca6a26a1e
Paulo Adson <paulo.alves200581@gmail.com>
2024-04-02 14:22:49 -0300

    resolução do curso

--- exercicios-js/exercicio4.js
+++ exercicios-js/exercicio4.js
@@ -2,33 +2,74 @@
 function mesDoAno(numeroDoMesDoAno) {
     switch (numeroDoMesDoAno) {
         case 1:
-            return "Janeiro"
+            return "Janeiro";
         case 2:
-            return "Fevereiro"
+            return "Fevereiro";
         case 3:
-            return "Março"
+            return "Março";
         case 4:
-            return "Abril"
+            return "Abril";
         case 5:
-            return "Maio"
+            return "Maio";
         case 6:
-            return "Junho"
+            return "Junho";
         case 7:
-            return "Julho"
+            return "Julho";
         case 8:
-            return "Agosto"
+            return "Agosto";
         case 9:
-            return "Setembro"
+            return "Setembro";
         case 10:
-            return "Outubro"
+            return "Outubro";
         case 11:
-            return "Novembro"
+            return "Novembro";
         case 12:
-            return "Dezembro"
+            return "Dezembro";
 
-        default: 
-        return 'Insira o número do Mês'
+        default:
+            return 'Insira o número do Mês'
     }
 }
 
 console.log(mesDoAno(1));
+
+// Resolução do Curso
+function receberNomeDoMes(numero) {
+    switch (numero) {
+        case 1:
+            return "Janeiro";
+        case 2:
+            return "Fevereiro";
+        case 3:
+            return "Março";
+        case 4:
+            return "Abril";
+        case 5:
+            return "Maio";
+        case 6:
+            return "Junho";
+        case 7:
+            return "Julho";
+        case 8:
+            return "Agosto";
+        case 9:
+            return "Setembro";
+        case 10:
+            return "Outubro";
+        case 11:
+            return "Novembro";
+        case 12:
+            return "Dezembro";
+    }
+}
+
+console.log(receberNomeDoMes(5));
+
+// Resolução do Curso 2
+function receberNomeDoMes2(numero) {
+    const mapeamento = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
+
+    return mapeamento [--numero];
+}
+
+console.log(receberNomeDoMes2(1));
