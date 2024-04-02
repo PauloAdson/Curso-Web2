cbc5ed86d21bf5c818f3e3ca3f079ccd48d7af50
Paulo Adson <paulo.alves200581@gmail.com>
2024-04-02 17:43:33 -0300

    resolução do curso

--- exercicios-js/exercicio6.js
+++ exercicios-js/exercicio6.js
@@ -11,4 +11,16 @@ function inverso(vaiSerInverso) {
     }
 }
 
-console.log(inverso(0));
+console.log(inverso('0'));
+
+//Resolução do Curso
+function inverso2(valor) {
+    const tipo = typeof valor
+
+    if (tipo == "boolean") return !valor
+    else if (tipo == "number") return -valor
+    else
+        return `booleano ou número esperados, mas o parâmetro é do tipo ${tipo}`
+}
+
+console.log(inverso2(false));
