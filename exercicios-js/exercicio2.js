2ccbb4e44ef876345f0038b089a5ca13b48ae45b
Paulo Adson <paulo.alves200581@gmail.com>
2024-04-02 11:09:19 -0300

    exercicio 2

---
+++ exercicios-js/exercicio2.js
@@ -0,0 +1,16 @@
+// Minha resolução
+function converterIdadeAnosParaDias(anos) {
+    const diasParaAnos = anos * 365
+    return diasParaAnos
+}
+
+console.log(converterIdadeAnosParaDias(17));
+
+//  Resolução do Curso
+function converterIdadeEmAnosParaDias(idadeEmAnos) {
+    const diasDoAno = 365
+
+    return diasDoAno * idadeEmAnos
+}
+
+console.log(converterIdadeAnosParaDias(17));
