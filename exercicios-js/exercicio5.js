a08274a272675c033e8dd72dd8f2d55d24ab82f8
Paulo Adson <paulo.alves200581@gmail.com>
2024-04-02 15:52:57 -0300

    resolvendo o exercício 5

---
+++ exercicios-js/exercicio5.js
@@ -0,0 +1,9 @@
+// Minha Resolução
+function maiorOuIgual(numero1, numero2) {
+    const ehIgual = numero1 === numero2
+    return ehIgual
+}
+
+console.log(maiorOuIgual(1, 1));
+
+// Resolução do Curso
