844e49aeb07274964d38f47a60af8d2a888fcb56
Paulo Adson <paulo.alves200581@gmail.com>
2024-04-02 22:24:11 -0300

    aprendendo mais sobre node

---
+++ node/moduloB.js
@@ -0,0 +1,7 @@
+// Outra forma de exportar, caso eu for exportar uma função, devo fazer assim module.exports {}
+module.exports = {
+    bomDia: 'Bom dia',
+    boaNoite () {
+        return 'Boa noite'
+    }
+}
---
+++ node/moduloCliente.js
@@ -0,0 +1,12 @@
+// Formas de fazer o import de modulos
+const moduloA = require('./moduloA')
+const moduloB = require('./moduloB')
+
+console.log(moduloA.ola);
+console.log(moduloA.bemVindo);
+console.log(moduloA.ateLogo);
+console.log(moduloA);
+
+console.log(moduloB.bomDia);
+console.log(moduloB.boaNoite());
+console.log(moduloB);
